d734e896ff24a17b3d9281bc3783bc2c2d6a9405
Ramiros73 <Ramiros73@gmail.com>
2023-01-06 13:00:02 +0400

    Initial Commit

---
+++ Lesson 2.02 Stack View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 2.02 Stack View
+//
+//  Created by Рамиль Хамидуллин on 06.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
