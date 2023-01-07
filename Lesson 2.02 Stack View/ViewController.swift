3cb4104015676910455bf14b804a339f09556982
Ramiros73 <Ramiros73@gmail.com>
2023-01-08 00:07:06 +0400

    setup settings of traffic light

--- Lesson 2.02 Stack View/ViewController.swift
+++ Lesson 2.02 Stack View/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var buttonView: UIButton!
+    private var currentColor = "red"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redView.alpha = 0.1
+        yellowView.alpha = 0.1
+        greenView.alpha = 0.1
+                
+        redView.layer.cornerRadius = 55
+        yellowView.layer.cornerRadius = 55
+        greenView.layer.cornerRadius = 55
+        buttonView.setTitle("Start", for: .normal)
+        
     }
-
-
-}
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+        
+            if buttonView.currentTitle == "Start" {
+                buttonView.setTitle("Next", for: .normal)
+        }
+            if currentColor == "red" {
+                redView.alpha = 1
+                greenView.alpha = 0.1
+                currentColor = "yellow"
+                
+            } else if currentColor == "yellow" {
+                redView.alpha = 0.1
+                yellowView.alpha = 1
+                currentColor = "green"
+                
+            } else if currentColor == "green" {
+                yellowView.alpha = 0.1
+                greenView.alpha = 1
+                currentColor = "red"
+            }
+        }
+    }
+    
 
